93829c0fdaf5cb36d5b2bee4e54a335c7b2ab47b
Mark <staslive2014@gmail.com>
2026-01-02 14:45:34 +0300

    v4.0 Изучение директивов

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script lang="ts">
 import Vehicle from '@/components/Vehicle.vue';
 
-
-
-export default{
-components:{
-  Vehicle
-},
+export default {
+  components: {
+    Vehicle
+  },
+  data() {
+    return {
+      isVisible: true
+    };
+  }
 }
 </script>
 
 <template>
   <Vehicle />
 
-  <h1>Транспортное средство </h1>
-
-
-  
+  <h1>Транспортное средство</h1>
+  <p v-if="isVisible">Вы меня видите!</p>
+  <p v-else>А теперь нет.</p>
+  <button @click="isVisible = !isVisible">Переключить</button>
 </template>
